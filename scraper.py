3e3e27d2c49a1fac1a93d36de4151e5b0022b36a
Jessica <jessil46@uci.edu>
2024-04-17 12:48:59 -0700

    Changed is_valid

--- scraper.py
+++ scraper.py
@@ -31,23 +31,20 @@ def extract_next_links(url, resp):
     return links
 
 def is_valid(url):
-    # Decide whether to crawl this url or not. 
-    # If you decide to crawl it, return True; otherwise return False.
-    # There are already some conditions that return False.
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
-
+        if not re.match(
+            r".*\.(ics|cs|informatics|stat)\.uci\.edu.*", parsed.netloc):
+            return False
+        if re.search(r".*\.(css|js|bmp|gif|jpeg|jpg|ico|png|tiff|pdf"
+                     r"|mp3|mp4|avi|mov|mpeg|tar|gz|zip|rar)$", parsed.path.lower()):
+            return False
+        # Remove URL fragments
+        if parsed.fragment:
+            return False
+        return True
     except TypeError:
-        print ("TypeError for ", parsed)
+        print("TypeError for URL:", url)
         raise
